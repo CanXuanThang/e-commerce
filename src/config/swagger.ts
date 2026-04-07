42b3576a1424e110b498b2a3289d40d6d1a0e279
thangcx <canxuanthang2001tt@gmail.com>
2026-04-07 12:16:28 +0700

    fix: server swagger

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -10,7 +10,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: "http://localhost:3000/api/v1",
+        url: "https://e-commerce-production-eaaf.up.railway.app//api/v1",
       },
     ],
   },
